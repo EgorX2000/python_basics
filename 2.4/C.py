a3f9c000dd85d4144cfa25e56658a7f6b5b33fc3
EgorX2000 <ebkrivoshapkin@yandex.ru>
2023-09-29 21:58:27 +0300

    2.4 C

--- 2.4/C.py
+++ 2.4/C.py
@@ -1,13 +1,11 @@
 n = int(input())
-n1 = n
 
-length = 1
-k = 1
-while n1 > 0:
-    for i in range(min(length, n1)):
-        print(k, end=" ")
-        k += 1
-    print()
-
-    n1 -= length
-    length += 1
+current = target = 0
+for i in range(1, n + 1):
+    if current == target:
+        print(i)
+        current = 0
+        target += 1
+    else:
+        print(i, end=" ")
+        current += 1
